b2be190369536932672ffa629afaf6b00f8a5288
Bruno Lacerda <brunolcpg@gmail.com>
2020-06-29 20:24:38 -0300

    Add method to delete book

--- src/main/java/com/brunopego/library/api/resource/BookController.java
+++ src/main/java/com/brunopego/library/api/resource/BookController.java
@@ -41,6 +41,13 @@ public class BookController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        Book book = service.getById(id).get();
+        service.delete(book);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrors handleValidationExceptions(MethodArgumentNotValidException e) {
